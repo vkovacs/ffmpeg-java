1723798431e1a9cab20715297171e53798f83ab7
Viktor Kovacs <viktor.kovacs@siemens.com>
2024-05-15 15:26:00 +0200

    get width / height

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,18 @@ package org.example;
 
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_videoio.VideoCapture;
+import org.opencv.highgui.HighGui;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.opencv.core.CvType.CV_8UC1;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        int height = 240;
-        int width = 320;
-
-        Mat frame = new Mat(height, width, CV_8UC1);
-
+    public static void main(String[] args) {
         try (VideoCapture capture = new VideoCapture("C:\\TMP\\video\\sample_video_roof.mp4")) {
+            var width = capture.get(3);
+            var height = capture.get(4);
+            Mat frame = new Mat(height, width, CV_8UC1);
+
             int i = 0;
 
             boolean hasNextFrame;
