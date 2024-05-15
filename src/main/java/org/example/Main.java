05de66bbc6b68219ddc5f962be34a0898a74f0b7
Viktor Kovacs <viktor.kovacs@siemens.com>
2024-05-15 15:35:24 +0200

    get bytes from frames

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,13 +2,22 @@ package org.example;
 
 import org.bytedeco.opencv.opencv_core.Mat;
 import org.bytedeco.opencv.opencv_videoio.VideoCapture;
-import org.opencv.highgui.HighGui;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.bytedeco.opencv.global.opencv_imgcodecs.imwrite;
 import static org.opencv.core.CvType.CV_8UC1;
 
 public class Main {
     public static void main(String[] args) {
+        var images = images();
+        System.out.println(images);
+    }
+
+    private static List<byte[]> images() {
+        var list = new ArrayList<byte[]>();
+
         try (VideoCapture capture = new VideoCapture("C:\\TMP\\video\\sample_video_roof.mp4")) {
             var width = capture.get(3);
             var height = capture.get(4);
@@ -21,6 +30,7 @@ public class Main {
                 hasNextFrame = capture.read(frame);
                 if (!frame.empty()) {
                     imwrite("C:\\TMP\\video\\cover-%s.jpg".formatted(String.format("%04d", i)), frame);
+                    list.add(asByteArray(frame));
                 }
                 i++;
 
@@ -28,5 +38,12 @@ public class Main {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        return list;
+    }
+
+    private static byte[] asByteArray(Mat mat){
+        byte[] b = new byte[mat.channels() * mat.cols() * mat.rows()];
+        mat.data().get(b);
+        return b;
     }
 }
